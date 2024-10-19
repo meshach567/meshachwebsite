bc7e2ddd1b91bc94d4606e9b791bd7b83563900e
meshach567 <arinzemeshach123@gmail.com>
2024-10-19 18:13:32 +0100

    reedit the components folder and add the Hometsx file

---
+++ src/components/Home/home.tsx
@@ -0,0 +1,97 @@
+import './home.scss'
+import Kemi from '/images/creative-Kemi.png'
+import { useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters/animated'
+import '../AnimatedLetters/animated.scss'
+import { Link as ScrollLink } from 'react-scroll'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+    const [letterClass] = useState('text-animate')
+    const nameArray = [
+        'e',
+        'm',
+        'i',
+        ' ',
+        'O',
+        'l',
+        'u',
+        'w',
+        'a',
+        'd',
+        'a',
+        'h',
+        'u',
+        'n',
+        's',
+        'i',
+    ]
+    const jobArray = [
+        'W',
+        'e',
+        'b',
+        ' ',
+        'D',
+        'e',
+        'v',
+        'e',
+        'l',
+        'o',
+        'p',
+        'e',
+        'r',
+        '.',
+    ]
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>
+                    <br />
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>&apos;m</span>
+                    <img src={Kemi} alt="introduction" className="k-letter" />
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={nameArray}
+                        idx={15}
+                    />
+                    <br />
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={jobArray}
+                        idx={22}
+                    />
+                </h1>
+
+                <h2>Frontend Developer / Code Enthusiast</h2>
+
+                <div className="buttons" >
+                    <Link to="services">
+                        <ScrollLink to="portfolioSection" smooth={true} duration={500}>
+                            <button className="flat-button">See my Latest Works</button>
+                        </ScrollLink>
+
+                        <ScrollLink to="contact" smooth={true} duration={500}>
+                            <button className="flat-button">CONTACT ME</button>
+                        </ScrollLink>
+
+                    </Link>
+                </div>
+            </div>
+
+            <div className="imageContainer">
+                <img
+                    className="my-image"
+                    src="https://res.cloudinary.com/dee9teadk/image/upload/v1717402807/my-image3_yc3f2q.png"
+                    alt="Kemi"
+                    loading="lazy"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Home
